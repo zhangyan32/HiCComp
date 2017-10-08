140ebc8f5a0b35a82269eb2a9dbd242365d44e90
Yan Zhang <Yan@Xiyas-MacBook-Pro.local>
2017-10-08 15:48:19 -0700

    delete unrelated py files

--- untitled.py
+++
@@ -1,7 +0,0 @@
-
-
-
-
-feature_map_dl = np.save(path + '/K562vsGM12878_chr18_size100_step10_Pearson_important_samples', feature_map)
-feature_map_pixel = np.load(path + '/K562vsGM12878_chr18_size100_step10_Pixel_important_samples.npy')
-feature_map_pearson = np.load(path + '/K562vsGM12878_chr18_size100_step10_CNN_important_samples.npy')
